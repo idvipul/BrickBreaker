6f8dca69c7da7f0dd6d9f1c1925bb7d76a2a5887
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-04-04 15:26:49 -0700

    JFrame added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        JFrame obj = new JFrame();
+
+        obj.setSize(800, 600);
+//        obj.setBounds(10,10,800,600);
+        obj.setTitle("Brick Breaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
