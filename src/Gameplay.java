a4144b8b5ec9b0f1ceb6e3ee65260d86d972370a
Vipul Karanjkar <vipulkaranjkar94@gmail.com>
2018-04-05 01:54:48 -0700

    moving slider

--- src/Gameplay.java
+++ src/Gameplay.java
@@ -1,11 +1,46 @@
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import java.awt.*;
 
-public class Gameplay extends JPanel {
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
+import javax.swing.Timer;
+
+// KeyListener --> detect arrow movements
+// ActionListener --> detect ball movement
+public class Gameplay extends JPanel implements KeyListener, ActionListener{
+    // add some properties
+    private boolean play = false;
+    private int score = 0;
+
+    //  3 cols * 7 rows
+    private int totalBricks = 21;
+
+    // to set time to the ball -- speed of the ball
+    private Timer timer;
+    // timer speed
+    private int delay = 8;
+
+    // starting pos of the slider
+    private int playerX = 310;
+
+    // starting position of ball
+    private int ballposX = 120;
+    private int ballposY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
 
     // default constructor
-    public Gameplay() { }
+    public Gameplay() {
+        // detect arrow movements
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
 
     // to draw different shapes
     public void paint(Graphics g) {
@@ -15,20 +50,59 @@ public class Gameplay extends JPanel {
 
         // borders;
         g.setColor(Color.yellow);
-        // upper border
-        g.fillRect(0, 0, 692, 3);
-        // left border
         g.fillRect(0, 0, 3, 592);
-        // right border
+        g.fillRect(0, 0, 692, 3);
         g.fillRect(691, 0, 3, 592);
 
         // slider
         g.setColor(Color.green);
-        g.fillRect(310, 550, 100, 8);
+        g.fillRect(playerX, 550, 100, 8);
 
         // ball
         g.setColor(Color.yellow);
-        g.fillRect(120, 350, 20, 20);
+        g.fillOval(ballposX, ballposY, 20, 20);
 
+        g.dispose();
     }
+
+    // Action Listener Method
+    public void actionPerformed (ActionEvent e) {
+        timer.start();
+        repaint();
+    }
+
+    // Key Event Methods:
+    @Override
+    public void keyTyped(KeyEvent e) { }
+    public void keyReleased(KeyEvent e) { }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerX >= 600) {
+                // if playerX goes beyond border, keep it at border
+                playerX = 600;
+            } else {
+                moveRight();
+            }
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerX < 10) {
+                // if playerX goes beyond border, keep it at border
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+        }
+    }
+
+    public void moveRight() {
+        play = true;
+        playerX = playerX + 20;
+    }
+
+    public void moveLeft() {
+        play = true;
+        playerX = playerX - 20;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,9 @@ public class Main {
 
         // create JFrame
         obj.setSize(700, 730);
-//        obj.setBounds(10,10,700,600);
+//        obj.setBounds(10,10,700,730);
         obj.setTitle("Brick Breaker");
-        obj.setResizable(true);
+//        obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
